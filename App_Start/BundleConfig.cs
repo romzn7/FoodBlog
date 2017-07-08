c15d334b220db4dd1c9484a6a425659add36b87e
romzn7 <rabbz123@gmail.com>
2017-07-08 13:47:57 +0545

    added ajax and jquery codes for searching and pagination

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace blog
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/otf.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using blog.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +11,31 @@ namespace blog.Controllers
     public class HomeController : Controller
     {
         OdeToFoodDb _db = new OdeToFoodDb();
-        public ActionResult Index(string searchTerm = null)
+
+        public ActionResult AutoComplete(string term)
+        {
+            var model = _db.Restuarants
+                            .Where(r => r.Name.StartsWith(term))
+                            .Take(10)
+                            .Select(r => new
+                            {
+                                label = r.Name
+                            });
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index(string searchTerm = null, int page = 1)
         {
             var model = _db.Restuarants
                             .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
                             .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
-                            .Take(10)
-                            .Select (r => new RestuarantListViewModel
+                            .Select(r => new RestuarantListViewModel
                             {
                                 Id = r.id,
                                 Name = r.Name,
                                 City = r.City,
                                 Country = r.Country,
                                 CountOfReviews = r.Reviews.Count()
-                            });
+                            }).ToPagedList(page, 10);
             if (Request.IsAjaxRequest())
             {
                 return PartialView("_Restuarants", model);
